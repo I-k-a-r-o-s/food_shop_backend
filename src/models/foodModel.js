1fe6f2010779f21dee4a862dc434af95f350125f
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-24 15:32:03 +0530

    wrote food model

---
+++ src/models/foodModel.js
@@ -0,0 +1,28 @@
+import { model, models, Schema } from "mongoose";
+
+const foodSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+const FoodModel = models.FoodModel || model("FoodModel", foodSchema);
+
+export default FoodModel;
