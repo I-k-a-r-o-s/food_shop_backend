8d54c43de9c5b13d21647d65a7a46132b9ffc07a
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-24 15:13:34 +0530

    connection to mongodb

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import { connectMongoDB } from "./config/mongodb.js";
 
 //server config
 const server = express();
 const port = process.env.PORT;
 
 //middleware
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
 
-
+//connect to db and start server
 const serverStartLog = () => {
   console.log(`Server is running on Port:- ${port}`);
 };
 const startserver = async () => {
   try {
+    connectMongoDB(); //mongodb connection
     server.listen(port, serverStartLog());
   } catch (error) {
     console.error("Error in startServer!:", error);
