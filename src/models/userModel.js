43f3ed6c7b51d57d13322a5b5f24e48a5398e879
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-27 09:35:15 +0530

    wrote user model

---
+++ src/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false },//keep empty object in mongodb
+);
+
+const UserModel =
+  mongoose.models.UserModel || mongoose.model("UserModel", userSchema);
+
+export default UserModel;
